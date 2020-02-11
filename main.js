d5239851bb81a6bcad7916aba3bbd3bc0fb53a9e
David Lau <David.lau@codeimmersives.com>
2020-02-10 19:34:57 -0500

    updated

---
+++ main.js
@@ -0,0 +1,38 @@
+document.body.style.backgroundImage = 'url(/bck.jpg)'
+const paragraph = document.querySelector('p')
+paragraph.style.fontSize = '5em'
+paragraph.style.color = 'pink'
+const h1 = document.querySelector('h1')
+h1.style.color = 'blue'
+h1.style.transform = 'rotate(180deg)'
+
+
+
+const header3 = document.querySelectorAll('h3')
+header3[1].style.transform = 'rotate(90deg)'
+header3[1].style.fontSize = '10em'
+header3[0].style.color = 'green'
+const myul = document.querySelector('ol')
+const myList = document.querySelectorAll('li')
+myList[0].style.fontSize = '2em'
+myList[1].style.fontSize = '10em'
+myList[2].style.fontSize = '5em'
+myList[1].style.color = 'red'
+myList[4].style.color = 'purple'
+myList[6].style.color = 'green'
+myList[2].style.color = 'blue'
+myList[0].style.textAlign = 'center'
+myList[2].style.border = "thick solid yellow";
+myList[7].style.textAlign = 'center'
+myList[8].style.textAlign = 'right'
+myul.style.border = "thick solid #0000FF";
+myList[5].style.border = "solid green";
+myList[6].style.border = "solid orange";
+myList[7].style.border = "solid red";
+myList[8].style.border = "solid yellow";
+myList[8].style.transform = 'rotate(90deg)'
+myList[4].style.transform = 'rotate(120deg)'
+myList[5].style.transform = 'rotate(150deg)'
+myList[5].style.backgroundColor = 'pink'
+myList[2].style.backgroundColor = 'brown'
+myList[8].style.backgroundColor = 'lightblue' 
